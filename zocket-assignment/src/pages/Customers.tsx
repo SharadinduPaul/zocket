218491d3051ef1c069668a28b5b2dc95db6d50de
SharadinduPaul <sharadindu.paul007@gmail.com>
2023-02-04 12:16:05 +0530

    page text changed

--- zocket-assignment/src/pages/Customers.tsx
+++ zocket-assignment/src/pages/Customers.tsx
@@ -4,7 +4,7 @@ import { PageNotAvailable } from "../components/pages/PageNotAvailable";
 export const Customers = () => {
   return (
     <div style={{ flex: 1, position: "relative" }}>
-      <PageNotAvailable title="Ugh, I only got 3 days. Not available!" />
+      <PageNotAvailable title="Ugh.. I only got 3 days. Page Not available!" />
     </div>
   );
 };
